46319f587743d00174ab2305a2bad3d6ca4e3ac7
younGi <k.younghwan18@googlemail.com>
2015-07-01 12:29:36 +0200

    some trial data and mainview

---
+++ myData.js
@@ -0,0 +1,22 @@
+myData([
+{"cars": [
+        { 
+            "id":0, 
+    		"name":"Max",
+    		"rang":4
+        },
+        {
+            "id":1, 
+    		"name":"Markus",
+    		"rang":2
+        },{
+            "id":2, 
+    		"name":"Young-Hwan",
+    		"rang":3
+        },{
+            "id":3, 
+    		"name":"Young-Hwan",
+    		"rang":1
+        }
+        ]
+}])
