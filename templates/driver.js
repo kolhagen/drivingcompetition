8b2b333549efdcc37bfe0cce94557cbd186dbfe0
Max <mkolhagen@gmail.com>
2015-07-07 17:50:28 +0200

    Added Buttons for Date-Selection

--- templates/driver.js
+++ templates/driver.js
@@ -2,6 +2,15 @@
  * Classes representing the list of drivers.
  */
 var DriverList = React.createClass({
+	getInitialState: function() {
+		return {month: TODAY.getMonth(), year: TODAY.getFullYear()};
+	},
+	onChangeYear: function(year) {
+		this.setState({year: year});
+	},
+	onChangeMonth: function(month) {
+		this.setState({month: month});
+	},
 	render: function() {
 		// build driver entries
 		var driverNodes = this.props.data.map(function (driver, i) {
@@ -13,9 +22,50 @@ var DriverList = React.createClass({
 			);
 		}.bind(this));
 
+		var yearNodes = [];
+		for (var year = FIRST_YEAR; year <= TODAY.getFullYear(); year++) {
+			yearNodes.push((
+				<li key={year}><a tabIndex="-1" href="#" onClick={this.onChangeYear.bind(null, year)}>{year}</a></li>
+			));
+		}
+
+		var monthNodes = [];
+		var maxMonth = 12;
+		if (this.state.year == TODAY.getFullYear())
+			maxMonth = TODAY.getMonth();
+
+		for (var month = 0; month < maxMonth; month++) {
+			monthNodes.push((
+				<li key={month}><a tabIndex="-1" href="#" onClick={this.onChangeMonth.bind(null, month)}>{MONTHS[month]}</a></li>
+			));
+		}
+
 		return (
-			<div id="driver-list">
-				{driverNodes}
+			<div>
+				<div className="btn-toolbar">
+					<div className="btn-group">
+						<a className="btn btn-info dropdown-toggle" data-toggle="dropdown" href="#">
+							{this.state.year}
+							<span className="caret"></span>
+						</a>
+						<ul className="dropdown-menu">
+							{yearNodes}
+						</ul>
+					</div>
+
+					<div className="btn-group">
+						<a className="btn btn-info dropdown-toggle" data-toggle="dropdown" href="#">
+							{MONTHS[this.state.month]}
+							<span className="caret"></span>
+						</a>
+						<ul className="dropdown-menu">
+							{monthNodes}
+						</ul>
+					</div>
+				</div>
+				<div id="driver-list">
+					{driverNodes}
+				</div>
 			</div>
 		);
 	}
@@ -30,3 +80,24 @@ var Driver = React.createClass({
 		);
 	}
 });
+
+
+/*
+
+	<ul class="nav nav-pills">
+		<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				Dropdown
+				<span class="caret"></span>
+			</a>
+			<ul class="dropdown-menu">
+				<li><a href="#">Action</a></li>
+				<li><a href="#">Another action</a></li>
+				<li><a href="#">Something else here</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="#">Separated link</a></li>
+			</ul>
+		</li>
+	</ul>
+
+*/
