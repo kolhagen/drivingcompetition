dce389be37abcef17234f8e7d04b1bab98cb6415
Markus Braun <markus.braun87@gmail.com>
2015-07-07 14:36:14 +0200

    links changes color on mouseclick

--- javascript/details.js
+++ javascript/details.js
@@ -11,44 +11,45 @@ function GetURLParams(sParam) {
 
 // trigger for list
 
-$(document).ready(
-		function() {
-			var json = (function () {
-			    var json = null;
-			    $.ajax({
-			        'async': false,
-			        'global': false,
-			        'url': "data/details_example_data.json",
-			        'dataType': "json",
-			        'success': function (data) {
-			            json = data;
-			        }
-			    });
-			    return json;
-			})();
-			$('#list a').click(
-					function() {
-						var ele = $(this).children().attr("id");
-						$("#trips").html("<p>Trips : "+json.details.detail[ele].trips+"</p>");
-						$("#fuel_consumed").html("<p>Fuel consumed : "+json.details.detail[ele].fuel_consumed+"</p>");
-						$("#fuel_saved").html("<p>Fuel Saving : "+json.details.detail[ele].fuel_savings+"</p>");
+$(document)
+		.ready(
+				function() {
+					var json = (function() {
+						var json = null;
+						$.ajax({
+							'async' : false,
+							'global' : false,
+							'url' : "data/details_example_data.json",
+							'dataType' : "json",
+							'success' : function(data) {
+								json = data;
+							}
 						});
-			
-			console.log(json);
-			
-			});
-//						alert (driver);
-//			var jsonObj = $.getJSON(
-//					"data/details_example_data.json",
-//					function(json) {
-//						return $.parseJSON(json);
-//					});
-//			console.log(jsonObj);
-//			var myjson = window.Object.responseJSON;
-//			console.log (myjson);
-//						console.log(jsonObj);
-						// $("#trips").append(json.details.detail[counter].trips);
-//					});
-//		});
-// alert (jsonObj.details.detail[0}.trips]);
-// })
+						return json;
+					})();
+					$('#list a')
+							.click(
+									function() {
+										$("#list a").each(function (){
+											$(this).removeClass("active");
+										});
+										$(this).addClass("active");
+										var ele = $(this).children().attr("id");
+										$("#trips")
+												.html(
+														"<p>Trips : "
+																+ json.details.detail[ele].trips
+																+ "</p>");
+										$("#fuel_consumed")
+												.html(
+														"<p>Fuel consumed : "
+																+ json.details.detail[ele].fuel_consumed
+																+ "</p>");
+										$("#fuel_saved")
+												.html(
+														"<p>Fuel Saving : "
+																+ json.details.detail[ele].fuel_savings
+																+ "</p>");
+									});
+
+				});
