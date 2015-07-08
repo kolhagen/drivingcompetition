d0a49e65b6e54c4c542a27344f1e2db7189f871d
Max <mkolhagen@gmail.com>
2015-07-08 15:58:20 +0200

    Removed cars attribute from drivers (not needed anymore)

--- data/driver.js
+++ data/driver.js
@@ -3,52 +3,33 @@ data.driver = [
 	{
 		name: "Markus Braun",
 		department: "Management",
-		cars: [
-			"sap.ctex::Audi_A6"
-		],
 		car: "system-local::WWWEPSILON5555555",
 		picture: "images/markus.png"
 	},
 	{
 		name: "Young-Hwan Kim",
 		department: "IT",
-		cars: [
-			"sap.ctex::BMW_3series",
-			"sap.ctex::Volkswagen_Golf"
-		],
 		car: "system-local::WWWBETA2222222222"
 	},
 	{
 		name: "Max Kolhagen",
 		department: "Accounting",
-		cars: [
-			"sap.ctex::Ford_Focus"
-		],
 		car: "system-local::WWWDELTA444444444"
 	},
 	{
 		name: "Kristin Boehm",
 		department: "Archive",
-		cars: [
-			"sap.ctex::BMW_5series"
-		],
 		car: "system-local::WWWALPHA111111111",
 		picture: "images/kristin.png"
 	},
 	{
 		name: "David Keller",
 		department: "Acquisitions",
-		cars: [
-			"sap.ctex::Mercedes-Benz_C-Class"
-		],
 		car: "system-local::WWWGAMMA333333333"
 	},
 	{
 		name: "Angela Merkel",
 		department: "Acquisitions",
-		cars: [
-			"sap.ctex::Mercedes-Benz_E-Class"
-		],
 		car: "system-local::WWWZETA6666666666"
 	}
 ]
