fa185acde3a551c53fd73ba998072e5c25e56818
Max <mkolhagen@gmail.com>
2015-07-08 15:11:49 +0200

    Added merging of extra user data

--- build/sap-data.js
+++ build/sap-data.js
@@ -1,9 +1,28 @@
 var SAP; if (!SAP) SAP = {};
 
 SAP.DATA = {
-  init: function() {
-    console.log("Initializing SAP Data...");
-  }
+	init: function() {
+		console.log("Initializing SAP Data...");
+		if (localStorage.getItem("data") === null) {
+			console.error("Could not load SAP user data!");
+			return;
+		}
+
+		var data = JSON.parse(localStorage.getItem("data"));
+		SAP.DATA.mergeData(data);
+	},
+
+	mergeData: function(extra) {
+		for (driver of data.driver) {
+			if (!driver.extra)
+				driver.extra = {};
+
+			if (!extra[driver.car])
+				continue;
+
+			driver.extra = extra[driver.car];
+		}
+	}
 }
 
 SAP.DATA.init();
