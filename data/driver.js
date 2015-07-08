8cb85246307ca5ea382e2c203feddf76361a1a67
Markus Braun <markus.braun87@gmail.com>
2015-07-08 21:09:30 +0200

    added some Quotes

--- data/driver.js
+++ data/driver.js
@@ -4,37 +4,42 @@ data.driver = [
 		name: "Markus Braun",
 		department: "Management",
 		car: "system-local::WWWEPSILON5555555",
-		picture: "images/markus.jpg"
+		picture: "images/markus.jpg",
+		quote: "Wer später bremst fährt länger schnell!"
 	},
 	{
 		name: "Young-Hwan Kim",
 		department: "IT",
 		car: "system-local::WWWBETA2222222222",
-		picture: "images/youngi.jpg"
+		picture: "images/youngi.jpg",
+		quote: "Wer bremst verliert!"
 	},
 	{
 		name: "Max Kolhagen",
 		department: "Accounting",
 		car: "system-local::WWWDELTA444444444",
-		picture: "images/max.png"
+		picture: "images/max.png",
+		quote: "Der rote Bereich beginnt dort wo der Mut aufhört!"
 	},
 	{
 		name: "Super Mario",
 		department: "Archive",
 		car: "system-local::WWWALPHA111111111",
-		picture: "images/mario.png"
+		picture: "images/mario.png",
+		quote: "Auch ein roter Panzer kann mich nicht aufhalten."
 	},
 	{
 		name: "Sebastian Vettel",
 		department: "Acquisitions",
 		car: "system-local::WWWGAMMA333333333",
-		picture: "images/vettel.png"
+		picture: "images/vettel.png",
+		quote: "Beim Beschleunigen müssen die Tränen der Ergriffenheit waagrecht zum Ohr hin abfließen"
 	},
 	{
 		name: "Marco Reus",
 		department: "Acquisitions",
 		car: "system-local::WWWZETA6666666666",
-		picture: "images/angi.jpg",
-		picture: "images/marco.png"
+		picture: "images/marco.png",
+		quote: "Wer braucht schon einen Führerschein?"
 	}
 ]
